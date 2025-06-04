800288d7d9307c50ae46ddf1a8394f065d334ddf
binh20032912@gmail.com <Ngbinh2912>
2025-06-05 02:42:59 +0700

    new enemy (ghost, skull, slime2)

---
+++ Assets/Scripts/BulletType1.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BulletType1 : MonoBehaviour
+{
+    public float speed = 8f;
+    public float damage = 5f;
+    public float lifeTime = 3f;
+
+    private Vector2 moveDirection;
+
+    public void SetDirection(Vector2 dir)
+    {
+        moveDirection = dir.normalized;
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.takeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Slime2.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Slime2 : Enemy
+{
+    [SerializeField] private Collider2D weaponCollider;
+
+    protected override void Start()
+    {
+        base.Start();
+        if (weaponCollider != null)
+        {
+            weaponCollider.enabled = false;
+        }
+    }
+
+    public void EnableWeaponCollider()
+    {
+        if (weaponCollider != null)
+        {
+            weaponCollider.enabled = true;
+        }
+    }
+
+    public void DisableWeaponCollider()
+    {
+        if (weaponCollider != null)
+        {
+            weaponCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && weaponCollider.enabled)
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.takeDamage(enterDamage);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.takeDamage(stayDamage);
+            }
+        }
+    }
+}
